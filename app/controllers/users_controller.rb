bc1be34c9bd6e3409e110c7eb1e4cd0fbccdb674
abe555 <riho.has3v@gmail.com>
2020-08-02 18:35:22 +0900

    Fix sigin_error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:update]
 
-  def new
-  end
-
   def show
     @user = User.find(params[:id])
     @books = @user.books
